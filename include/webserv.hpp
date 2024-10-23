47e372d53a3905265536d9053052802fc2ec4312
ys4ber <youssefsaber1337@gmail.com>
2024-10-23 16:49:18 +0100

    adding the basic files

---
+++ include/webserv.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <unistd.h>      // execve, dup, dup2, pipe, fork, chdir, access, read, write, close
+#include <cstring>      // strerror, gai_strerror
+#include <cerrno>       // errno
+#include <sys/types.h>  // socket, accept, listen, send, recv, bind, connect, setsockopt, getsockname, getprotobyname, fcntl, stat, opendir, readdir, closedir
+#include <sys/socket.h> // socket, accept, listen, send, recv, bind, connect, setsockopt, getsockname, socketpair
+#include <netinet/in.h> // htons, htonl, ntohs, ntohl
+#include <netdb.h>      // getaddrinfo, freeaddrinfo, getprotobyname
+#include <sys/select.h> // select
+#include <poll.h>       // poll
+#include <sys/epoll.h>  // epoll_create, epoll_ctl, epoll_wait
+#include <signal.h>     // signal, kill
+#include <sys/wait.h>   // waitpid
+#include <fcntl.h>      // fcntl
+#include <dirent.h>     // opendir, readdir, closedir
+#include <sys/stat.h>   // stat
+#include <fstream>      // ifstream
+#include <sstream>      // istringstream
+#include <cstdlib>      // atoi
+#include <map>
+#include <vector>
+#include <deque>
+#include <list>
+
+
---
+++ srcs/main.cpp
@@ -0,0 +1,13 @@
+#include "include/webserv.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        std::cerr << "Usage: " << av[0] << " <config_file>" << std::endl;
+        return 1;
+    }
+    
+
+
+}
