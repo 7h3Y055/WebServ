9d2a41aa220faf264790d1c98f140b1f83052b51
bl4kcy <mostafafarssiui@gmail.com>
2024-11-05 18:02:20 +0100

    feat: Refactor delete_Response function to simplify parameters and improve file handling

--- srcs/methods/_Delete/delete_Response.cpp
+++ srcs/methods/_Delete/delete_Response.cpp
@@ -1,21 +1,11 @@
 #include "webserv.hpp"
-#include "utils.cpp"
-#include "response_returns.cpp"
-#ifndef _DELETE_RESPONSE_HPP
-#define _DELETE_RESPONSE_HPP
-using namespace std;
 
-string root = "/home/melfersi/cc/WebServ/";
-
-string path1 = "css";// dir
-string path2 = "/css/folder"; // file
-
-
-Response	*delete_Response(Request *req, string path)
+Response	*delete_Response(Request *req)
 {
 	Response *res = new Response(*req);
 	// get_request_source(req);
-	string source = root + path;
+	// string source = root + path1;
+	string source = req->get_file_name();
 	cout << source << endl;
 	if (access(source.c_str(), F_OK) == -1)
 	{
@@ -34,4 +24,3 @@ Response	*delete_Response(Request *req, string path)
 	}
 	return res;
 }
-#endif
