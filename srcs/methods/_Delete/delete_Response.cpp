00dbb6ccd055f96eaff7db5fd1cb125ef65994ab
bl4kcy <mostafafarssiui@gmail.com>
2024-11-08 12:08:37 +0100

    feat: Simplify source path construction in delete_Response and add debug output

--- srcs/methods/_Delete/delete_Response.cpp
+++ srcs/methods/_Delete/delete_Response.cpp
@@ -2,16 +2,19 @@
 
 void	delete_Response(Request *req)
 {
-	string source = servers[req->get_server_index()].getRoot() + get_location(req->get_file_name(), servers[req->get_server_index()]).getPath();
+	string source = servers[req->get_server_index()].getRoot() + req->get_file_name();
 
+	cout << "source: " << source << endl;
 	if (access(source.c_str(), F_OK) == -1)
 		throw 404;
+	cout << "source: " << source << endl;
 	if (is_source_a_directory(source))
 	{
+		cout << "source: " << source << endl;
+		cout << "delete_Response: source is a directory" << endl;
 		if (delete_directory(source.c_str()))
 			throw 204; // No Content
 	}
 	else if (is_source_a_file(source))
 		delete_file(source.c_str());
-	
 }
